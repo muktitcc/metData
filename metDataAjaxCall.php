883574025950380a8c69076b6fe11e6d2882aace
Mukti Nath Chhetri <muktitcc@gmail.com>
2016-03-31 14:52:03 +0600

    Update metDataAjaxCall.php

--- metDataAjaxCall.php
+++ metDataAjaxCall.php
@@ -2,16 +2,16 @@
 error_reporting(E_ALL);
 ini_set('memory_limit', '-1');
 require('inc/config.php');
-include('ssp.class.php');
+require('ssp.class.php');
 $table = 'tblmhvmetdata';
 $primaryKey = 'trnid';
 $columns = array(
-    array( 'db' => 'trnid', 'dt' => 0 ),
-    array( 'db' => 'station_id',     'dt' => 1 ),
-    array('db' => 'mdate',        'dt'        => 2),
-	array('db' => 'celsius',        'dt'        => 3),
-	array('db' => 'humidity',        'dt'        => 4),
-	array('db' => 'dewpoint',        'dt'        => 5),
+    	array( 'db' => 	'trnid', 		'dt' => 0 ),
+    	array( 'db' => 	'station_id',     	'dt' => 1 ),
+    	array('db' => 	'mdate',        	'dt'        => 2),
+	array('db' => 	'celsius',        	'dt'        => 3),
+	array('db' => 	'humidity',        	'dt'        => 4),
+	array('db' => 	'dewpoint',        	'dt'        => 5),
 );
 echo "daf";
 echo json_encode(SSP::simple( $_GET, $connInfo, $table, $primaryKey, $columns ));
