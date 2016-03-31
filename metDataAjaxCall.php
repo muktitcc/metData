b7746c213aeb1c8511b8dbfc570a04504bee75ce
Mukti Nath Chhetri <muktitcc@gmail.com>
2016-03-31 14:29:10 +0600

    Update metDataAjaxCall.php

--- metDataAjaxCall.php
+++ metDataAjaxCall.php
@@ -1,6 +1,5 @@
 <?php
 ini_set('memory_limit', '-1');
-echo "1";
 //require_once('inc/config.php');
 $connInfo = array(
     'user' => 'root',
@@ -8,9 +7,7 @@ $connInfo = array(
     'db'   => 'metstation',
     'host' => 'localhost'
 );
-echo "2";
 include('ssp.class.php');
-echo "3";
 $table = 'tblmhvmetdata';
 $primaryKey = 'trnid';
 $columns = array(
@@ -21,14 +18,5 @@ $columns = array(
 	array('db' => 'humidity',        'dt'        => 4),
 	array('db' => 'dewpoint',        'dt'        => 5),
 );
- 
-
- 
-
- 
-echo json_encode(
-    SSP::simple( $_GET, $connInfo, $table, $primaryKey, $columns )
-);
-	
-
+echo json_encode(SSP::simple( $_GET, $connInfo, $table, $primaryKey, $columns ));
 ?>
