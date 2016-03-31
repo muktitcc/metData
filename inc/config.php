5e0accf70c3e1602f132d6bc662bec8ccf9c60b2
mukti <mchhetri@mountainhazelnuts.com>
2016-03-31 13:43:52 +0600

    live
    d

--- inc/config.php
+++ inc/config.php
@@ -1,16 +1,9 @@
 <?php
 $connInfo = array(
-<<<<<<< HEAD
     'user' => 'root',
     'pass' => 'ty1977',
     'db'   => 'metstation',
     'host' => 'localhost'
-=======
-    'user' => '',
-    'pass' => '',
-    'db'   => '',
-    'host' => ''
->>>>>>> origin/master
 );
 $conn = mysql_connect($connInfo['host'], $connInfo['user'], $connInfo['pass']) or die ("Error connecting to database");
 mysql_select_db($connInfo['db'],$conn);
