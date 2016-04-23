096503ccda971ba642de7e3bfb91f2a283a0be37
jngituser <rmruk@btinternet.com>
2016-04-23 12:14:47 +0100

    Create formgetdistalt.php
    Find MH metstations and RGOB met stations that are near each other
    and within specified altitude difference. Enter specified distance and altitude difference and submit. Get processed by stationlatlongdist.php

---
+++ formgetdistalt.php
@@ -0,0 +1,8 @@
+<p>Find MH metstations and RGOB met stations that are near each other
+and within specified altitude difference. Enter specified distance and altitude difference and submit. Get processed by stationlatlongdist.php</p>
+
+<form method="get" action = "stationlatlongdist.php">
+	Enter maximum distance apart for stations in km: <input type="text" name="dist"/><br/>
+	Enter maxium altitude difference for stations in metres: <input type="text" name="altdiff"/<br/>
+	<input type="submit" name ="submit" value="Find stations"/>
+</form>
