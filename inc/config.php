40abf4577e3f6b39fa79212601f14ebb57dc9071
Mukti Nath Chhetri <muktitcc@gmail.com>
2016-04-29 14:26:08 +0600

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -8,6 +8,6 @@ $connInfo = array(
 $conn = mysql_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]) or die ("Error connecting to database");
 $link = mysql_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]) or die ("Error connecting to database"); // Janny's connection variable
 mysql_select_db($connInfo["db"],$conn);
-mysql_select_db($connInfo["db"],$link); // for Janny's connection variable $link
+mysqli_select_db($connInfo["db"],$link); // for Janny's connection variable $link
 
 ?>
