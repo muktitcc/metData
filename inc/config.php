57093ae074b2d3546cc06d97bbe4daa09852a206
Mukti Nath Chhetri <muktitcc@gmail.com>
2016-04-29 15:11:32 +0600

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -6,5 +6,5 @@ $connInfo = array(
     'host' => 'localhost'
 );
 $conn = mysql_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]) or die ("Error connecting to database");
-mysql_select_db($connInfo["db"],$conn);
+mysqli_select_db($connInfo["db"],$conn);
 ?>
