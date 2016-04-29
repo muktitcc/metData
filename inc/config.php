7e496a5119707214329c99738b05f52266cb6f0c
Mukti Nath Chhetri <muktitcc@gmail.com>
2016-04-29 15:11:52 +0600

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -5,6 +5,6 @@ $connInfo = array(
     'db'   => 'metstation',
     'host' => 'localhost'
 );
-$conn = mysql_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]) or die ("Error connecting to database");
+$conn = mysqli_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]) or die ("Error connecting to database");
 mysqli_select_db($connInfo["db"],$conn);
 ?>
