cfa7ea63c964bd442e39bbc498271baf28cc3fb8
jngituser <rmruk@btinternet.com>
2016-06-07 15:15:54 +0100

    Create mhvchillseasoncalc.php

---
+++ mhvchillseasoncalc.php
@@ -0,0 +1,61 @@
+<!-- calculate chill hours by season for rgob metdata -->
+
+<style>
+	tr:nth-child(even){
+		background-color:#d3d3d3;
+	}
+	tr:nth-child(1){
+		background-color:#808080;
+	}
+</style>
+
+<?php
+
+require_once( "inc/config.php" );// common connection variable is $conn
+
+$link = mysqli_connect($connInfo["host"], $connInfo["user"], $connInfo["pass"]); // common db connection variable
+mysqli_select_db($link,$connInfo["db"]);       // common db connection variable   
+     
+
+
+echo "<table>";
+	echo "<tr><th>Station</th><th>Season</th><th>Lower chillhrs</th><th>Middle chillhrs</th><th>Upper chillhrs</th><th>Count</th>
+	</tr>";
+
+
+$sql = "SELECT CH.station_id AS station, S.season AS season, SUM(CH.lowbandchillhrs), SUM(CH.midbandchillhrs), SUM(CH.upbandchillhrs), COUNT(*)       
+ FROM tblmhvchillhours  CH
+ INNER JOIN tblchillseasons S
+         ON CH.mdate >= S.startDate 
+        and CH.mdate <=  S.endDate
+ GROUP BY CH.station_id, S.season";      //get the MHV chillhours
+											
+$result2 = mysqli_query($link,$sql);
+
+while($row=mysqli_fetch_array($result2)){
+
+	$rgobstation_id = $row["station"];                //
+	$rgobdate = $row["season"]; 
+	$lowerch = $row["SUM(CH.lowbandchillhrs)"];
+	$middlech = $row["SUM(CH.midbandchillhrs)"];
+	$upperch = $row["SUM(CH.upbandchillhrs)"];
+	$numb = $row["COUNT(*)"];
+ 
+	
+		echo "<tr>						
+			<td>$rgobstation_id
+			<td>$rgobdate
+			<td>$lowerch
+			<td>$middlech
+			<td>$upperch
+			<td>$numb
+									
+		</tr>";
+
+}
+echo "</table>";
+
+
+mysqli_close($link);
+?>
+	
