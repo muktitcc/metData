fe7085b3334478199751db2c3645c1b3ad2f48ef
jngituser <rmruk@btinternet.com>
2016-09-29 16:12:06 +0100

    Update calcrgobchillhours.php

--- calcrgobchillhours.php
+++ calcrgobchillhours.php
@@ -53,7 +53,7 @@ echo "<table>";
 $sql = "SELECT * FROM tblrgobAmetdata where tmax IS NOT NULL and tmin IS NOT NULL and (month(bdate) NOT BETWEEN 4 AND 10)";      //get the RGOB met stations Tmax and Tmin for months Nov to Mar
 											//if there is a NULL tmax or tmin row will not be selected ... need to address this
 $result2 = mysqli_query($link,$sql);
-$chilhrs = 24
+$chillhrs = 24
 
 while($row=mysqli_fetch_array($result2)){
 
@@ -82,7 +82,7 @@ if($tmax == $tmin){  					//deal with tmax and tmin being equal not to divide by
 
 $total = ($lowerch+$middlech+$upperch);
 
-$sql_insert = "INSERT INTO tblrgobchillhours (station_id, bdate, lowbandchillhrs, midbandchillhrs, upbandchillhrs, chillhrs) VALUES ('$rgobstation_id', '$rgobdate', '$lowerch', '$middlech', '$upperch', '$chilhrs')";
+$sql_insert = "INSERT INTO tblrgobchillhours (station_id, bdate, lowbandchillhrs, midbandchillhrs, upbandchillhrs, counthrs) VALUES ('$rgobstation_id', '$rgobdate', '$lowerch', '$middlech', '$upperch', '$chillhrs')";
 mysqli_query($link,$sql_insert);
 	
 		echo "<tr>						
